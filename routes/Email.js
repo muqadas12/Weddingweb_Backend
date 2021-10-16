0f3eb20f58c679760875d3646102ecaf2fb8a89a
muqadas12 <60438885+muqadas12@users.noreply.github.com>
2021-10-16 10:57:24 +0500

    email-enhancement

--- routes/Email.js
+++ routes/Email.js
@@ -20,19 +20,24 @@ app.post('/mail', function (req, res, next) {
         `;
 
     var transport = nodemailer.createTransport({
-      service: 'gmail',
+      // service: 'gmail',
+      // host: 'smtp.gmail.com',
+      // port: 587,
+      // secure: false,
       host: 'smtp.gmail.com',
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
-        user: 'muqadasshaban@gmail.com',
-        pass: 'momina20',
+        // user: 'muqadasshaban@gmail.com',
+        // pass: 'momina20',
+        user: 'muqaddasshaaban@gmail.com',
+        pass: 'vejulrhditiysqds',
       },
     });
 
     var mailoption = {
       from: req.body.name,
-      to: 'muqadasshaban@gmail.com',
+      to: 'muqaddasshaaban@gmail.com',
       subject: 'New msg',
       rec: req.body.email,
       text: req.body.msg,
