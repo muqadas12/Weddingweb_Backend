2fc2cb5da07b2deb2f48c9090706171934f49303
muqadas12 <60438885+muqadas12@users.noreply.github.com>
2021-10-18 18:29:00 +0500

    final

--- routes/DealerRoute.js
+++ routes/DealerRoute.js
@@ -41,8 +41,7 @@ const postDealerdata = app.post(
     dealer.description = req.body.description;
     dealer.price = req.body.price;
     dealer.email = req.body.email;
-    dealer.pathImg =
-      'https://wedding-web-app.herokuapp.com/' + req.file.filename;
+    dealer.pathImg = 'http://localhost:2000/static/' + req.file.filename;
     console.log(dealer.pathImg);
     dealer.img.contentType = 'image/png';
     dealer.save((err, result) => {
